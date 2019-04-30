420fef2ca5de488e4c7db0af99f9e0d6d95c4e68
Eberson Cosme <eberson.cosme@pravaler.com.br>
2019-04-30 14:30:24 -0300

    Removendo component core e migrando dados para o shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import{FormsModule, ReactiveFormsModule} from '@angular/forms';
 
@@ -6,10 +6,21 @@ import {InputComponent} from './input/input.component';
 import {RadioComponent} from './radio/radio.component';
 import {RatingComponent} from './rating/rating.component';
 
+import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import {RestaurantsService} from '../restaurants/restaurants.service';
+import {OrderService} from '../order/order.service';
+
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [InputComponent, RadioComponent, RatingComponent,
             CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders { /*Quando chamado carrega juntamente os providers */
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    };
+  }
+}
