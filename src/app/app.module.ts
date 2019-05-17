bc1f66bdc800e34b8b78b89ca4bf9f39ff3b3ad9
Eberson Cosme <eberson.cosme@pravaler.com.br>
2019-05-17 16:41:55 -0300

    Ajustes finais no error-handle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, LOCALE_ID} from '@angular/core';
+import {NgModule, LOCALE_ID, ErrorHandler} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, PreloadAllModules} from '@angular/router';
 
+import { ApplicationErrorHandler } from './app.error-handler';
 import {ROUTES} from './app.routes';
 
 import {AppComponent} from './app.component';
@@ -46,7 +47,8 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'},
+              {provide: ErrorHandler, useClass: ApplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
