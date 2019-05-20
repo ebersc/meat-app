c7e61285345e45c7dcba2fd80a8459744a28b6fd
Eberson Cosme <eberson.cosme@pravaler.com.br>
2019-05-20 11:17:26 -0300

    Alteraçao para nova forma de criaçao de formularios

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,5 +1,5 @@
 import {HttpErrorResponse} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+// import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 
